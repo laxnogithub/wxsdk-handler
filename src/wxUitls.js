49f584ae33bed66b0684fff9f8e794dec1b8fac6
lax <lax263@zrtg.com>
2020-04-09 17:26:22 +0800

    update

--- src/wxUitls.js
+++ src/wxUitls.js
@@ -4,14 +4,16 @@
  * @Autor: lax
  * @Date: 2020-04-08 10:38:49
  * @LastEditors: lax
- * @LastEditTime: 2020-04-08 10:39:55
+ * @LastEditTime: 2020-04-09 10:01:00
  */
 
 const axios = require("axios");
 
 const wxsdk = wx;
 
-const /* 默认申请API权限项 */
+const 
+  
+  /* 默认申请API权限项 */
   DEFAULT_API_LIST = [
     "updateAppMessageShareData",
     "updateTimelineShareData",
@@ -21,50 +23,53 @@ const /* 默认申请API权限项 */
     "onMenuShareQZone",
     "onMenuShareWeibo"
   ],
-  /* 默认执行完成回调函数 */
 
+  /* 默认执行完成回调函数 */
   DEFAULT_OVER = function() {
     console.log("wx plugin is ready!");
   },
+
   /* 默认DEBUG模式 */
-
   DEFAULT_DEBUG_STATE = false,
+
+  /* 默认环境 */
+  DEFAULT_PRO_STATE = false,
+
   /* 默认标题 */
-
   DEFAULT_TITLE = document.title,
+
   /* 默认描述 */
-
   DEFAULT_DESC = DEFAULT_TITLE,
+
   /* 默认链接地址 */
-
   DEFAULT_LINK = location.href,
+
   /* 默认图片 */
-
   DEFAULT_IMG = "",
+
   /* 默认后端服务器地址 */
-
   DEFAULT_SERVER_URL = "https://wx.server.1045fm.cn",
+
   /* 默认后端测试服务器地址 */
-
   DEFAULT_TEST_SERVER_URL = "http://172.27.14.236:8800",
-  /* 微信授权地址 */
 
+  /* 微信授权地址 */
   AUTH_URL =
     "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect",
+  
   /* 默认测试用APPID */
-
   DEFAULT_TEST_APPID = "wx884d34049d77fcd1",
+  
   /* 默认生成环境APPID */
-
   DEFAULT_PRO_APPID = "wxbfaae54e7f89f3fa",
+  
   /* 默认后端服务器接口地址 */
-
   DEFAULT_REDIRECT_URI_PATH = "/wx/redirect",
+  
   /* 默认状态参数 */
-
   // DEFAULT_STATE = "",
-  /* 默认请求类型 */
 
+  /* 默认请求类型 */
   DEFAULT_SCOPE_TYPE = ["snsapi_base", "snsapi_userinfo"];
 
 function wxProcessor(p) {
@@ -72,46 +77,17 @@ function wxProcessor(p) {
   let self_url = location.href.split("#")[0].toString(),
     self = this;
 
-  /**
-   * @member {boolean} debug
-   * @memberof wx
-   * @inner
-   * @default false
-   */
   this.debug = p.debug || DEFAULT_DEBUG_STATE;
 
-  /**
-   * @member {boolean} index
-   * @memberof wx
-   * @inner
-   * @ignore
-   */
+  this.pro = p.pro || DEFAULT_PRO_STATE;
+
   this.indexUrl = p.index || self_url;
 
-  /**
-   * @member {string} server
-   * @memberof wx
-   * @inner
-   * @default dev/pro
-   */
   this.server =
-    p.server || (this.debug ? DEFAULT_TEST_SERVER_URL : DEFAULT_SERVER_URL);
+    p.server || (this.pro ? DEFAULT_SERVER_URL : DEFAULT_TEST_SERVER_URL);
+    
+  this.appid = p.appid || (this.pro ? DEFAULT_PRO_APPID : DEFAULT_TEST_APPID);
 
-  /**
-   * @member {string} appid
-   * @memberof wx
-   * @inner
-   * @default dev/pro
-   */
-  this.appid = p.appid || (this.debug ? DEFAULT_TEST_APPID : DEFAULT_PRO_APPID);
-
-  /**
-   * @member {string} scope
-   * @memberof wx
-   * @inner
-   * @default snsapi_userinfo
-   * @description snsapi_base or snsapi_userinfo
-   */
   this.scope =
     typeof p.scope == "string"
       ? p.scope
