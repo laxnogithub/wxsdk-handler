26107d3ee6ceaad0dba61edbfb43ae6613d4e262
lax <lax263@zrtg.com>
2020-09-17 17:50:28 +0800

    update

--- src/wxUitls.js
+++ src/wxUitls.js
@@ -4,98 +4,40 @@
  * @Autor: lax
  * @Date: 2020-04-08 10:38:49
  * @LastEditors: lax
- * @LastEditTime: 2020-07-06 14:40:45
+ * @LastEditTime: 2020-09-17 16:20:38
  */
 
+const path = require('path');
+const DEFAULT = require(path.join(__dirname,'./default'));
 const axios = require("axios");
 
 const wxsdk = wx;
 
-const 
-  
-  /* 默认申请API权限项 */
-  DEFAULT_API_LIST = [
-    "updateAppMessageShareData",
-    "updateTimelineShareData",
-    "onMenuShareTimeline",
-    "onMenuShareAppMessage",
-    "onMenuShareQQ",
-    "onMenuShareQZone",
-    "onMenuShareWeibo"
-  ],
-
-  /* 默认执行完成回调函数 */
-  DEFAULT_OVER = function() {
-    console.log("wx plugin is ready!");
-  },
-
-  /* 默认DEBUG模式 */
-  DEFAULT_DEBUG_STATE = false,
-
-  /* 默认生产环境 */
-  DEFAULT_PRO_STATE = false,
-
-  /* 默认标题 */
-  DEFAULT_TITLE = document.title,
-
-  /* 默认描述 */
-  DEFAULT_DESC = DEFAULT_TITLE,
-
-  /* 默认链接地址 */
-  DEFAULT_LINK = location.href,
-
-  /* 默认图片 */
-  DEFAULT_IMG = "",
-
-  /* 默认后端服务器地址 */
-  DEFAULT_SERVER_URL = "https://wx.server.1045fm.cn",
-
-  /* 默认后端测试服务器地址 */
-  DEFAULT_TEST_SERVER_URL = "https://wxt.server.1045fm.cn",
-
-  /* 微信授权地址 */
-  AUTH_URL =
-    "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect",
-  
-  /* 默认测试用APPID */
-  DEFAULT_TEST_APPID = "wx884d34049d77fcd1",
-  
-  /* 默认生产环境APPID */
-  DEFAULT_PRO_APPID = "wxbfaae54e7f89f3fa",
-  
-  /* 默认后端服务器接口地址 */
-  DEFAULT_REDIRECT_URI_PATH = "/wx/redirect",
-  
-  /* 默认状态参数 */
-  // DEFAULT_STATE = "",
-
-  /* 默认请求类型 */
-  DEFAULT_SCOPE_TYPE = ["snsapi_base", "snsapi_userinfo"];
-
-function wxProcessor(p) {p = p || {};
+function wxProcessor(p= {}) {
   const self = this;
 
-  this.debug = p.debug || DEFAULT_DEBUG_STATE;
+  // 仅当默认为false可以这样写
+  this.debug = p.debug || DEFAULT.DEBUG_STATE;
 
-  this.pro = p.pro || DEFAULT_PRO_STATE;
+  this.pro = p.pro || DEFAULT.PRO_STATE;
 
   this.indexUrl = p.index || getURL(true);
 
   this.server =
-    p.server || (this.pro ? DEFAULT_SERVER_URL : DEFAULT_TEST_SERVER_URL);
+    p.server || (this.pro ? DEFAULT.SERVER_URL : DEFAULT.TEST_SERVER_URL);
     
-  this.appid = p.appid || (this.pro ? DEFAULT_PRO_APPID : DEFAULT_TEST_APPID);
+  this.appid = p.appid || (this.pro ? DEFAULT.PRO_APPID : DEFAULT.TEST_APPID);
 
   this.scope =
     typeof p.scope == "string"
       ? p.scope
       : typeof p.scope == "number"
-        ? DEFAULT_SCOPE_TYPE[p.scope]
-        : DEFAULT_SCOPE_TYPE[1];
+        ? DEFAULT.SCOPE_TYPE[p.scope]
+        : DEFAULT.SCOPE_TYPE[1];
 
   this.trigger = p.trigger ? p.trigger : null;
 
-  this.over = p.over || DEFAULT_OVER;
+  this.over = p.over || DEFAULT.OVER;
 
   this.updateByWxSDK = updateByWxSDK;
 
@@ -209,7 +151,7 @@ function wxProcessor(p) {p = p || {};
    */
   this.auth = function() {
     let url = AUTH_URL.replace("APPID", this.appid)
-      .replace("REDIRECT_URI", this.server + DEFAULT_REDIRECT_URI_PATH)
+      .replace("REDIRECT_URI", this.server + DEFAULT.REDIRECT_URI_PATH)
       .replace("SCOPE", this.scope)
       .replace("STATE", this.indexUrl);
     window.location.href = url;
@@ -257,13 +199,13 @@ function _getImg(path){
 
 function updateByWxSDK(p){
   p = p || {};
-    this.title = p.title || DEFAULT_TITLE;
-    this.debug = p.debug || DEFAULT_DEBUG_STATE;
-    this.desc = p.desc || DEFAULT_DESC;
-    this.link = p.link || DEFAULT_LINK;
-    this.img = _getImg(p.img || DEFAULT_IMG);
-    this.jsApiList = p.list || DEFAULT_API_LIST;
-    this.over = p.over || DEFAULT_OVER;
+    this.title = p.title || DEFAULT.TITLE;
+    this.debug = p.debug || DEFAULT.DEBUG_STATE;
+    this.desc = p.desc || DEFAULT.DESC;
+    this.link = p.link || DEFAULT.LINK;
+    this.img = _getImg(p.img || DEFAULT.IMG);
+    this.jsApiList = p.list || DEFAULT.API_LIST;
+    this.over = p.over || DEFAULT.OVER;
     this.trigger = p.trigger || null;
     return this;
 }
