9afc94d409dd23daf3d3c7cb7c87e05ec66a75fb
lax <lax263@zrtg.com>
2020-11-23 16:49:58 +0800

    update

--- examples/main.js
+++ examples/main.js
@@ -1,20 +1,28 @@
 /*
  * @Description:
  * @Version: 1.0.0
- * @Autor: lax
+ * @Author: lax
  * @Date: 2020-04-08 08:48:09
  * @LastEditors: lax
- * @LastEditTime: 2020-10-20 11:02:20
+ * @LastEditTime: 2020-11-23 11:45:09
  */
 import Vue from "vue";
 import App from "./App.vue";
 import wxHandler from "../packages/wx.js";
 
 Vue.config.productionTip = false;
+const isDev = process.env.NODE_ENV === "development";
 
 Vue.use(wxHandler, {
 	configFile: require("./../wx.js"),
-	config: { debug: false },
+	config: isDev
+		? {
+				debug: true,
+				appid: "wxbfaae54e7f89f3fa",
+				server: "https://wxt.server.1045fm.cn/",
+				path: "wx/sign",
+		  }
+		: {},
 });
 new Vue({
 	render: (h) => h(App),
